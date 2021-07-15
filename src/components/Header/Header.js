83c0890e430e557f39a949db39a8ce268cd734fc
williamrr <william.rodriguez@alteru.co>
2021-07-15 14:56:23 -0500

    arreglos de detalles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import logoPK from "../../assets/imgs/logo pokemon.png"
 
 const Header = () => {
     const view = `
-        <span class="header-home-img"><img src="${logoPK}" alt="Logo"></span>
-        <span><a href="#">Contáctenos</a></span>
+        <span class="headerImg"><img src="${logoPK}" alt="Logo"></span>
+        <span class= "headerA"><a href="#">Contáctenos</a></span>
     `;
     return view;
 };
