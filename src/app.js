58f6ae1a6c4065e81f473fbcefccf9b960ef9450
williamrr <william.rodriguez@alteru.co>
2021-07-14 19:26:27 -0500

    se agregan las dependencias

---
+++ src/app.js
@@ -0,0 +1,1 @@
+import css from "./assets/css/index.css"
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js',
+    },
+    module: {
+        rules: [{
+                test: /\.(png|jpg|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                    },
+                }, ],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html',
+    })],
+};
